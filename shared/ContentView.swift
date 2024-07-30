983ae13734cfea47eb37ba3569562de73e8b47e6
Apple <>
2024-07-30 00:18:55 +0000

    Resubmission.

--- shared/ContentView.swift
+++ shared/ContentView.swift
@@ -1,5 +1,5 @@
 /*
-See LICENSE folder for this sample’s licensing information.
+See the LICENSE.txt file for this sample’s licensing information.
 
 Abstract:
 The main view that displays the distance to the paired device.
