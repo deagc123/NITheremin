457bdb4c5193631615c2872063f5ef2ff96c10f3
deagc123 <deagc123@outlook.com>
2025-05-24 12:24:00 +0800

    delete useless thing

---
+++ shared/AudioManager.swift
@@ -0,0 +1,9 @@
+//
+//  AudioManager.swift
+//  WatchNIDemo
+//
+//  Created by 白 on 2025/5/18.
+//  Copyright © 2025 Apple. All rights reserved.
+//
+
+import Foundation
---
+++ shared/NIThereminApp.swift
@@ -0,0 +1,9 @@
+//
+//  NIThereminApp.swift
+//  NITheremin
+//
+//  Created by 白 on 2025/5/24.
+//  Copyright © 2025 Apple. All rights reserved.
+//
+
+import Foundation
